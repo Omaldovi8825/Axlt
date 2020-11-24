f1e96ffe75bd5cf6dd80c8d76dcf717c160a6883
Omar Maldonado <omar.maldo.vi@gmail.com>
2020-11-23 19:50:55 -0600

    algo

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-console.log('hola')
+let botonPsico = document.getElementById('psico')
+let fondo = document.body
+let encabezado = document.getElementById('header')
+let pie = document.getElementById('footer')
+let rueda = document.getElementById('rueda')
+let logo = document.getElementById('logo-centro')
+let gota2 = document.getElementById('gota2')
+
+
+function ponteLoco(){
+    fondo.style.backgroundColor = 'black'
+    encabezado.style.backgroundColor = "#c53c82"
+    footer.style.backgroundColor = "#c53c82"
+    rueda.style.border = "25px solid #71dd28"
+    logo.style.backgroundImage = "url('../images/logos/psicoo.svg')"
+}
+
+function muevete(){
+    gota2.style.transform = 'rotate(180deg) translate(120%, 60%)'
+    gota2.style.opacity = '1'
+}
